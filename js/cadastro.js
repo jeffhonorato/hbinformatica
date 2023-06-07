0e24f2264185b21e6f57fdb1f4553dae4d34bd33
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-06-07 14:42:36 -0300

    Ajustes

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,9 +1,20 @@
 const recargas = document.querySelector(".js-cadastro-recargas");
+const impressoras = document.querySelector(".js-cadastro-impressoras");
+const setores = document.querySelector(".js-cadastro-setores");
+const meses = document.querySelector(".js-cadastro-meses");
+const data = document.querySelector('input[type="datetime-local"]');
 const btnSalvar = document.querySelector(".js-btn-cadastro-salvar");
 
 btnSalvar.addEventListener("click", (e) => {
-    e.preventDefault()
+    e.preventDefault();
+
     const recarga = recargas.value;
-    console.log(recarga)
-})
+    const impressora = impressoras.value;
+    const setor = setores.value;
+    const mes = meses.value;
+    const dataAtual = data.value;
+    
+
+    return console.log(recarga, impressora, setor, mes, dataAtual)
+});
 
