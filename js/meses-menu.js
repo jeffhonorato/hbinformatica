e71d9777e470df3f4959cac07ad1565449219bf5
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-06-21 11:58:28 -0300

    Ajustes

--- js/meses-menu.js
+++ js/meses-menu.js
@@ -4,11 +4,11 @@ var swiper = new Swiper(".mySwiper", {
     centeredSlides: true,
     slidesPerView: "auto",
     coverflowEffect: {
-      rotate: 50,
+      rotate: 4,
       stretch: 0,
-      depth: 100,
+      depth: 300,
       modifier: 1,
-      slideShadows: true,
+      slideShadows: false,
     },
     pagination: {
       el: ".swiper-pagination",
